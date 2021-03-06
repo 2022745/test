36a6fdf96edfd2473007779fa46c340b2c948ebc
HP <HP@DESKTOP-IGBM6G9>
2021-03-05 21:22:51 -0600

    1.1

--- src/Test/HolaMundo.java
+++ src/Test/HolaMundo.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Nombre: Serafin Rodriguez
+Matricula: 2022745
+*/
+
 package Test;
 
 /**
